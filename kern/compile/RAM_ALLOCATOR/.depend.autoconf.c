89d084f6b605e99906bb2d7529c0da5e76a96798
MicheledG <michele.digirolamo.to@gmail.com>
2017-04-09 14:21:55 +0200

    Implemented new ram_stealmem function

---
+++ kern/compile/RAM_ALLOCATOR/.depend.autoconf.c
@@ -0,0 +1,4 @@
+autoconf.o: ../../compile/RAM_ALLOCATOR/autoconf.c ../../include/types.h \
+ ../../include/kern/types.h includelinks/kern/machine/types.h \
+ includelinks/machine/types.h ../../include/lib.h ../../include/cdefs.h \
+ opt-noasserts.h ../../compile/RAM_ALLOCATOR/autoconf.h
